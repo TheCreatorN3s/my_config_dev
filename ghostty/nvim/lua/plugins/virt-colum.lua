784857380562ce4c578760a7238226cc660edda1
TheCreatorN3s <n3s.creator@gmail.com>
2026-01-07 08:45:43 -0400

    add configurations

---
+++ ghostty/nvim/lua/plugins/virt-colum.lua
@@ -0,0 +1,11 @@
+return {
+  "lukas-reineke/virt-column.nvim",
+  config = function()
+    require("virt-column").setup({
+      enabled = true,
+      char = "│",
+      virtcolumn = "130",
+      highlight = "@float",
+    })
+  end,
+}
